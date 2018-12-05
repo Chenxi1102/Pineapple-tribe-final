146d57fedb931232cad66fe9fc350873a6351f51
Keyi Zhong <kz25@uw.edu>
2018-12-04 20:53:37 -0800

    Revert "no graph"
    This reverts commit c6e0f6e0aa6bef478c276648ef4f7e99e470e14f.

--- server.R
+++ server.R
@@ -7,7 +7,7 @@ library(ggplot2)
 
 ## Define server logic required to draw graphs and chart
 shinyServer(function(input, output) {
-# read the data
+# reeding the data in
   both <- read.csv("marriage/both_sexes.csv", stringsAsFactors = FALSE)
   men <- read.csv("marriage/men.csv", stringsAsFactors = FALSE)
   women <- read.csv("marriage/women.csv", stringsAsFactors = FALSE)
